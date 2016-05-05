07e98647df0a73900e2f10e50f1ddf0dfd88ece0
Kenji <designer.kenji@gmail.com>
2016-05-04 22:15:37 -0400

    api setup

---
+++ movie_quotes_api.py
@@ -0,0 +1,25 @@
+
+import endpoints
+from protorpc import message_types
+from protorpc import messages
+from protorpc import remote
+
+from models import MovieQuote
+import main
+
+@endpoints.api(name="moviequotes", version="v1", description="Movie QUotes API")
+class MovieQuotesApi(remote.Service):
+    pass
+
+    #Methods returns a single object or query methods return a collection
+    @MovieQuote.method(name="moviequote.insert", path="moviequote/insert", http_method="POST")
+    def moviequote_insert(self, request):
+        my_quote = MovieQuote(parent=main.PARENT_KEY, quote=request.quote, movie=request.movie)
+        my_quote.put()
+        #one way to get parent key
+        
+        #request.put()
+        #no parent key here
+        return request
+
+app = endpoints.api_server([MovieQuotesApi], restricted = False)
